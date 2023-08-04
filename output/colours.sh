01abc9275a71e170bd4d13fdb58b8bec04b35f8e
Dave Holden <holden.dave@gmail.com>
2023-08-04 16:06:31 +0100

    Renamed / refactored Display => foutput

--- output/colours.sh
+++ output/colours.sh
@@ -16,207 +16,159 @@ else
   TERM_COLS=0
 fi
 
-colors() {
+# colors() {
   if [ -n TPUT_FOUND ]; then
     # Define foreground colours
 
-    FG_BLACK=$( tput setaf 0 );     FGB=$FG_BLACK;     # \033[30m
-    FG_RED=$( tput setaf 1 );       FGR=$FG_RED;       # \033[31m
-    FG_GREEN=$( tput setaf 2 );     FGG=$FG_GREEN;     # \033[32m
-    FG_YELLOW=$( tput setaf 3 );    FGY=$FG_YELLOW;    # \033[33m
-    FG_BLUE=$( tput setaf 4 );      FGBL=$FG_BLUE;     # \033[34m
-    FG_MAGENTA=$( tput setaf 5 );   FGM=$FG_MAGENTA;   # \033[35m
-    FG_CYAN=$( tput setaf 6 );      FGC=$FG_CYAN;      # \033[36m
-    FG_WHITE=$( tput setaf 7 );     FGW=$FG_WHITE;     # \033[37m
+    FG_BLACK=$( tput setaf 0 );     BLACK=$FG_BLACK;      # \033[30m
+    FG_RED=$( tput setaf 1 );       RED=$FG_RED;          # \033[31m
+    FG_GREEN=$( tput setaf 2 );     GREEN=$FG_GREEN;      # \033[32m
+    FG_YELLOW=$( tput setaf 3 );    YELLOW=$FG_YELLOW;    # \033[33m
+    FG_BLUE=$( tput setaf 4 );      BLUE=$FG_BLUE;        # \033[34m
+    FG_MAGENTA=$( tput setaf 5 );   MAGENTA=$FG_MAGENTA;  # \033[35m
+    FG_CYAN=$( tput setaf 6 );      CYAN=$FG_CYAN;        # \033[36m
+    FG_WHITE=$( tput setaf 7 );     WHITE=$FG_WHITE;      # \033[37m
 
     # Define background colours
-    BG_BLACK=$(tput setab 0);       BGB=$BG_BLACK;     # \033[40m
-    BG_RED=$(tput setab 1);         BGR=$BG_RED;       # \033[41m
-    BG_GREEN=$(tput setab 2);       BGG=$BG_GREEN;     # \033[42m
-    BG_YELLOW=$(tput setab 3);      BGY=$BG_YELLOW;    # \033[43m
-    BG_BLUE=$(tput setab 4);        BGBL=$BG_BLUE;     # \033[44m
-    BG_MAGENTA=$(tput setab 5);     BGM=$BG_MAGENTA;   # \033[45m
-    BG_CYAN=$(tput setab 6);        BGC=$BG_CYAN;      # \033[46m
-    BG_WHITE=$(tput setab 7);       BGW=$BG_WHITE;     # \033[47m
-    
-    RESET=$( tput sgr0 );           NORMAL=$RESET;    # \033[0m
+    BG_BLACK=$(tput setab 0);                             # \033[40m
+    BG_RED=$(tput setab 1);                               # \033[41m
+    BG_GREEN=$(tput setab 2);                             # \033[42m
+    BG_YELLOW=$(tput setab 3);                            # \033[43m
+    BG_BLUE=$(tput setab 4);                              # \033[44m
+    BG_MAGENTA=$(tput setab 5);                           # \033[45m
+    BG_CYAN=$(tput setab 6);                              # \033[46m
+    BG_WHITE=$(tput setab 7);                             # \033[47m
+    RESET=$( tput sgr0 );           NORMAL=$RESET         # \033[0m
+
   else
 
     # Define default foreground Colours
-    FG_BLACK=''                     FGB=$FG_BLACK;
-    FG_RED=''                       FGR=$FG_RED;
-    FG_GREEN=''                     FGG=$FG_GREEN;
-    FG_YELLOW=''                    FGY=$FG_YELLOW;
-    FG_BLUE=''                      FGBL=$FG_BLUE;
-    FG_MAGENTA=''                   FGM=$FG_MAGENTA;
-    FG_CYAN=''                      FGC=$FG_CYAN;
-    FG_WHITE=''                     FGW=$FG_WHITE;
+    FG_BLACK=''                     BLACK=$FG_BLACK;
+    FG_RED=''                       RED=$FG_RED;
+    FG_GREEN=''                     GREEN=$FG_GREEN;
+    FG_YELLOW=''                    YELLOW=$FG_YELLOW;
+    FG_BLUE=''                      BLUE=$FG_BLUE;
+    FG_MAGENTA=''                   MAGENTA=$FG_MAGENTA;
+    FG_CYAN=''                      CYAN=$FG_CYAN;
+    FG_WHITE=''                     WHITE=$FG_WHITE;
     
     # Define default background colours
-    BG_BLACK=''                     BGB=$BG_BLACK;
-    BG_RED=''                       BGR=$BG_RED;
-    BG_GREEN=''                     BGG=$BG_GREEN;
-    BG_YELLOW=''                    BGY=$BG_YELLOW;
-    BG_BLUE=''                      BGBL=$BG_BLUE;
-    BG_MAGENTA=''                   BGM=$BG_MAGENTA;
-    BG_CYAN=''                      BGC=$BG_CYAN;
-    BG_WHITE=''                     BGW=$BG_WHITE;
-
-    RESET=''                        NORMAL=$RESET;
+    BG_BLACK=''
+    BG_RED=''
+    BG_GREEN=''
+    BG_YELLOW=''
+    BG_BLUE=''
+    BG_MAGENTA=''
+    BG_CYAN=''
+    BG_WHITE=''
+    RESET='';                       NORMAL=$RESET;
   fi
-}
+# }
 
 # Colors / text-formats.sh
-text_formats() {
+# text_formats() {
 
   # Define text formatting
   if [ -n TPUT_FOUND ]; then
-    TXT_BOLD=$( tput bold );        TB=$TXT_BOLD;
-    TXT_DIM=$( tput dim );          TD=$TXT_DIM;
-    TXT_ITALICS=$( tput sitm );     TI=$TXT_ITALICS;
-    TXT_ULINE=$( tput smul );       TU=$TXT_ULINE;
-    TXT_BLINK=$( tput blink );      TBL=$TXT_BLINK;
-    TXT_INVERT=$( tput rev );       TIV=$TXT_INVERT;
-    TXT_INVISIBLE=$( tput invis );  TINV=$TXT_INVISIBLE;
+    TXT_BOLD=$( tput bold );        BOLD=$TXT_BOLD;
+    TXT_DIM=$( tput dim );          DIM=$TXT_DIM;
+    TXT_ITALICS=$( tput sitm );     ITALICS=$TXT_ITALICS;
+    TXT_ULINE=$( tput smul );       ULINE=$TXT_ULINE;
+    TXT_BLINK=$( tput blink );      BLINK=$TXT_BLINK;
+    TXT_INVERT=$( tput rev );       INVERT=$TXT_INVERT;
+    TXT_INVISIBLE=$( tput invis );  INVISIBLE=$TXT_INVISIBLE;
 
     RESET=$( tput sgr0 );           NORMAL=$RESET;    # \033[0m
   else
     # Define default text formatting
-    TXT_BOLD=''                     TB=$TXT_BOLD;
-    TXT_DIM=''                      TD=$TXT_DIM;
-    TXT_ITALICS=''                  TI=$TXT_ITALICS;
-    TXT_ULINE=''                    TU=$TXT_ULINE;
-    TXT_BLINK=''                    TBL=$TXT_BLINK;
-    TXT_INVERT=''                   TIV=$TXT_INVERT;
-    TXT_INVISIBLE=''                TINV=$TXT_INVISIBLE;
+    TXT_BOLD=''                     BOLD=$TXT_BOLD;
+    TXT_DIM=''                      DIM=$TXT_DIM;
+    TXT_ITALICS=''                  ITALICS=$TXT_ITALICS;
+    TXT_ULINE=''                    ULINE=$TXT_ULINE;
+    TXT_BLINK=''                    BLINK=$TXT_BLINK;
+    TXT_INVERT=''                   INVERT=$TXT_INVERT;
+    TXT_INVISIBLE=''                INVISIBLE=$TXT_INVISIBLE;
 
     RESET=''                        NORMAL=$RESET;
   fi
-}
-
-# Setup foreground colour functions
-RED() { printf "%s\\n" "${FG_RED}${1}${NORMAL}"; }
-GREEN() { printf "%s\\n" "${FG_GREEN}${1}${NORMAL}"; }
-YELLOW() { printf "%s\\n" "${FG_YELLOW}${1}${NORMAL}"; }
-BLUE() { printf "%s\\n" "${FG_BLUE}${1}${NORMAL}"; }
-MAGENTA() { printf "%s\\n" "${MAGENTA}${1}${NORMAL}"; }
-CYAN() { printf "%s\\n" "${CYAN}${1}${NORMAL}"; }
-WHITE() { printf "%s\\n" "${WHITE}${1}${NORMAL}"; }
-
-# Setup background colour functions
-BG_RED() { printf "%s\\n" "${BG_RED}${1}${NORMAL}"; }
-BG_GREEN() { printf "%s\\n" "${BG_GREEN}${1}${NORMAL}"; }
-BG_YELLOW() { printf "%s\\n" "${BG_YELLOW}${1}${NORMAL}"; }
-BG_BLUE() { printf "%s\\n" "${BG_BLUE}${1}${NORMAL}"; }
-BG_MAGENTA() { printf "%s\\n" "${BG_MAGENTA}${1}${NORMAL}"; }
-BG_CYAN() { printf "%s\\n" "${BG_CYAN}${1}${NORMAL}"; }
-BG_WHITE() { printf "%s\\n" "${BG_WHITE}${1}${NORMAL}"; }
-
-# Setup text formattion functions
-BOLD() { printf "%s\\n" "${TXT_BOLD}${1}${TXT_RESET}"; }
-DIM() { printf "%s\\n" "${TXT_DIM}${1}${TXT_RESET}"; }
-ITALICS() { printf "%s\\n" "${TXT_ITALICS}${1}${TXT_RESET}"; }
-UNDERLINE() { printf "%s\\n" "${TXT_ULINE}${1}${TXT_RESET}"; }
-BLINK() { printf "%s\\n" "${TXT_BLINK}${1}${TXT_RESET}"; }
-INVERT() { printf "%s\\n" "${TXT_INVERT}${1}${TXT_RESET}"; }
-INVISIBLE() { printf "%s\\n" "${TXT_INVISIBLE}${1}${TXT_RESET}"; }
+# }
 
 COLORS=( 
-  "RED" "GREEN" "YELLOW" "BLUE" "MAGENTA" "CYAN" "WHITE" 
+  "FG_RED" "FG_GREEN" "FG_YELLOW" "FG_BLUE" "FG_MAGENTA" 
+  "FG_CYAN" "FG_WHITE"  
   "BG_RED" "BG_GREEN" "BG_YELLOW" "BG_BLUE" "BG_MAGENTA" 
   "BG_CYAN" "BG_WHITE"
 )
 FORMATS=( "BOLD" "DIM" "ITALICS" "UNDERLINE" "BLINK" "INVERT" "INVISIBLE")
 
-Display() { 
-  # Attempts to return string formatted according to 
-  # COLORS / STYLES passed in,
-  # Any unmatched string is added to the "message" to be displayed
+Foutput() {
+  # return string formatted according to 
+  declare -a text
+  declare messages styles
 
-  declare -a text methods
-  declare message
-
-  # get arguments passed to method
-  args=( "$@" )
   # Ensure atleast one element is passed in - could be just "text"
-  if [ ${#args} -eq 0 ]; then
-    echo "no arguments"
+  if [ ${#} -eq 0 ]; then
+    echo "Error: ${FUNCNAME[0]} requires arguments"
+    echo -e "\n ${FUNCNAME[0]} \"Some text\""
+    echo " Or"
+    echo -e " ${FUNCNAME[0]} [COLORS / STYLES] \"Some text\""
+    echo " eg ${FUNCNAME[0]} FG_RED Bold \"Some text\""
+    echo
     exit 1
   fi
 
-  # get index + 1 of arguments passed to method
-  counter=1    
-  len=$(( ${#args} + 1 ))
-
-  while [ "$counter" -lt $len ]; do
+  while [ $# -gt 0 ]; do
     # get string element from array, per index counter
-    value=${args[$counter]}
-
+    value="${1}"
     # convert string element to Upper-Case
-    value_upper=$(tr '[:lower:]' '[:upper:]' <<< $value)
+    value_upper="$(tr '[:lower:]' '[:upper:]' <<< $value)"
 
     # Check if upper case color / format value exists in array
     if [[ "${COLORS[*]}" =~ "${value_upper}" || "${FORMATS[*]}" =~ "${value_upper}" ]]; then
       # de-dupe methods
-      if ! [[ " ${methods[*]} " =~ ${value_upper} ]]; then
-        methods+=( "$value_upper" )
+      if ! [[ " ${styles[*]} " =~ ${value_upper} ]]; then
+        styles+="${!value_upper}"
       fi
     else
-      text+=$value
+      text+=( "${value}" )
     fi
-    (( counter++ ))
-  done
 
-  # Join array elements, concatinate space delimited
-  response=$(IFS=' '; echo "${text[*]}")
-  
-  # Iterate over any methods
-  for method in "${methods[@]}"
-  do
-    response=$( $method $response )
+    shift
   done
+# Concatinate message elements, space delimited
+  message="$(IFS=' '; echo "${text[*]}")"
 
-  echo ${response}
+  # Apply styles to message and reset/normalize styling
+  message="${styles}${message}${NORMAL}"
+  echo ${message}
 }
 
 stripDisplay() {
   # Remove escape codes - colors / styles - from string.
-  # echo $(echo "$spaced" | sed  $'s/\033[[][^A-Za-z]*[A-Za-z]//g')
   echo "$(sed $'s/\033[[][^A-Za-z]*[A-Za-z]//g' <<< "$@")"
-
-  # https://stackoverflow.com/questions/19296667/remove-ansi-color-codes-from-a-text-file-using-bash
-  # sed -r "s/\x1B\[(([0-9]{1,2})?(;)?([0-9]{1,2})?)?[m,K,H,f,J]//g" file_name
-  # this command removes the special characters and color codes from the file
-
-  # these are some of ANSI codes: ESC[#;#H or ESC[#;#f moves cursor to line #, column # ESC[2J clear screen and home cursor ESC[K clear to end of line,
-
-  # note in case of clear code there is neither number nor semicolon ;
-
-  # agree with below comment: if the numbers are more than 2 digit kindly use this:
-
-  # sed -r "s/\x1B\[(([0-9]+)(;[0-9]+)*)?[m,K,H,f,J]//g" filename
-
-
-  # Also
-  # https://stackoverflow.com/questions/17998978/removing-colors-from-output
-  # https://stackoverflow.com/questions/58096998/how-to-remove-color-style-format-from-a-variable
 }
 
-function escape_chars() {
-    local content="${1}"
-    shift
+escape_chars() {
 
-    for char in "$@"; do
-        content="${content//${char}/\\${char}}"
-    done
+  local content="${1}"
+  shift
+  for char in "$@"; do
+    content="${content//${char}/\\${char}}"
+  done
 
-    echo "${content}"
+  echo "${content}"
 }
 
-function echo_var() {
+echo_var() {
     local var="${1}"
     local content="${2}"
     local escaped="$(escape_chars "${content}" "\\" '"')"
 
     echo "${var}=\"${escaped}\""
 }
+
+# (return 0 2>/dev/null) && {
+#     echo "COLOURS IS SOURCED"; 
+#     colors; 
+#     text_formats
+# } || echo "COLOURS NOT SOURCED"
